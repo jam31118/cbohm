9b8fe4b5491537c9dc85d65a89ab56162a100f7d
jam31118 <jam45@naver.com>
2019-02-12 20:01:56 +0900

    applied jacobian routine to a test script

--- test/in-sph-harm/main.cc
+++ test/in-sph-harm/main.cc
@@ -8,6 +8,7 @@
 
 typedef std::complex<double> m_t;
 
+
 void print_bar() {
   printf("\n----------------------\n\n");
 }
@@ -34,21 +35,23 @@ int main(int argc, char *argv[]) {
   //// Allocate memory for data
 
   // `r_p_arr`
-  double **r_p_arr = new double*[N_r_dim];
-  double *r_p_arr_1d = new double[N_r_dim*N_p];
-  for (int i_r_dim = 0; i_r_dim < N_r_dim; i_r_dim++) {
-    r_p_arr[i_r_dim] = r_p_arr_1d + i_r_dim * N_p;
+  double **r_p_vec_arr = new double*[N_p];
+  double *r_p_vec_arr_1d = new double[N_p*DIM_R];
+//  for (int i_r_dim = 0; i_r_dim < N_r_dim; i_r_dim++) {
+  for (int i_p = 0; i_p < N_p; i_p++) {
+    r_p_vec_arr[i_p] = r_p_vec_arr_1d + i_p * DIM_R;
   }
 
   // `v_p_arr`
-  double **v_p_arr = new double*[N_r_dim];
-  double *v_p_arr_1d = new double[N_r_dim*N_p];
-  for (int i_r_dim = 0; i_r_dim < N_r_dim; i_r_dim++) {
-    v_p_arr[i_r_dim] = v_p_arr_1d + i_r_dim * N_p;
+  double **v_p_vec_arr = new double*[N_p];
+  double *v_p_vec_arr_1d = new double[N_p*DIM_R];
+//  for (int i_r_dim = 0; i_r_dim < N_r_dim; i_r_dim++) {
+  for (int i_p = 0; i_p < N_p; i_p++) {
+    v_p_vec_arr[i_p] = v_p_vec_arr_1d + i_p * DIM_R;
   }
 
   // `v_p_ana_arr`
-  double v_p_ana_arr[N_r_dim][N_p];
+  double v_p_ana_arr[N_p][DIM_R];
 
   // `psi_arr`
   m_t **psi_arr = new m_t*[N_lm];
@@ -57,6 +60,9 @@ int main(int argc, char *argv[]) {
     psi_arr[i_lm] = psi_arr[0] + i_lm * N_rho;
   }
 
+  // array of jacobian
+  jac_t *jac_p_arr = new jac_t[N_p];
+  jac_t *jac_p_ana_arr = new jac_t[N_p];
 
 
   //// Initialization
@@ -64,9 +70,9 @@ int main(int argc, char *argv[]) {
   // `rho_p_arr`
   double rho_p_arr[N_p] = {0.5, 1.0, 2.1, 2.4};
   for (int i_p = 0; i_p < N_p; i_p++) {
-    r_p_arr[0][i_p] = rho_p_arr[i_p];
-    r_p_arr[1][i_p] = 0.3 * M_PI;
-    r_p_arr[2][i_p] = 0.0;
+    r_p_vec_arr[i_p][0] = rho_p_arr[i_p];
+    r_p_vec_arr[i_p][1] = 0.3 * M_PI;
+    r_p_vec_arr[i_p][2] = 0.0;
   }
 
   // Construct radial coordinate grid points `rho_arr`
@@ -94,9 +100,20 @@ int main(int argc, char *argv[]) {
 
   // Evaluate analytical results
   for (int i_p = 0; i_p < N_p; i_p++) {
-    v_p_ana_arr[0][i_p] = 0.0;
-    v_p_ana_arr[1][i_p] = 0.0;
-    v_p_ana_arr[2][i_p] = 1.0 / (r_p_arr[0][i_p] * sin(r_p_arr[1][i_p]));
+    v_p_ana_arr[i_p][0] = 0.0;
+    v_p_ana_arr[i_p][1] = 0.0;
+    v_p_ana_arr[i_p][2] = 1.0 / (r_p_vec_arr[i_p][0] * sin(r_p_vec_arr[i_p][1]));
+  }
+
+  // Evaluate analytical jacobian
+  for (int i_p = 0; i_p < N_p; i_p++) {
+    for (int i_row = 0; i_row < DIM_R-1; i_row++) {
+      for (int i_col = 0; i_col < DIM_R-1; i_col++) {
+        jac_p_ana_arr[i_p][i_row][i_col] = 0.0;
+      }
+    }
+    jac_p_ana_arr[i_p][2][0] = - 1.0 / r_p_vec_arr[i_p][0] * v_p_ana_arr[i_p][2];
+    jac_p_ana_arr[i_p][2][1] = - cos(r_p_vec_arr[i_p][1]) / sin(r_p_vec_arr[i_p][1]) * v_p_ana_arr[i_p][2];
   }
 
   
@@ -104,16 +121,26 @@ int main(int argc, char *argv[]) {
   //// Evaluate velocity vector for each particle
   int return_code = EXIT_FAILURE;
 
-  return_code = eval_v_p_arr_for_sph_harm_basis(
-      N_s, N_p, N_rho, N_lm,
-      (double **) r_p_arr, (const m_t **) psi_arr, 
-      rho_arr, l_arr, m_arr, rho_p_lim, 
-      (double **) v_p_arr);
+//  return_code = eval_v_p_arr_for_sph_harm_basis(
+//      N_s, N_p, N_rho, N_lm,
+//      (double **) r_p_arr, (const m_t **) psi_arr, 
+//      rho_arr, l_arr, m_arr, rho_p_lim, 
+//      (double **) v_p_arr);
 
+//  for (int i_p = 0; i_p < N_p; i_p++) {
+  return_code = eval_v_p_vec_arr_for_sph_harm_basis(
+      N_s, N_p, N_rho, N_lm,
+      (double **) r_p_vec_arr, (const m_t **) psi_arr, 
+      rho_arr, l_arr, m_arr, rho_p_lim, 
+      (double **) v_p_vec_arr, jac_p_arr);
   if (return_code != EXIT_SUCCESS) {
-    fprintf(stderr, "[ERROR] Failed to run 'eval_psi_and_dpsidx_arr()'");
-    return return_code;
+    return debug_mesg_with_code(
+        "Failed to run 'eval_psi_and_dpsidx_arr()'", return_code);
+//    fprintf(stderr, "[ERROR] Failed to run 'eval_psi_and_dpsidx_arr()'");
+//    return return_code;
   }
+
+//  }
 
 
 
@@ -164,21 +191,42 @@ int main(int argc, char *argv[]) {
   for (int i_p = 0; i_p < N_p; i_p++) {
     printf("%5d%15.5f%15.5f%15.5f%15.5f%15.5f%15.5f\n", 
         i_p, 
-        v_p_arr[0][i_p], v_p_arr[1][i_p], v_p_arr[2][i_p],
-        v_p_ana_arr[0][i_p], v_p_ana_arr[1][i_p], v_p_ana_arr[2][i_p]
+        v_p_vec_arr[i_p][0], v_p_vec_arr[i_p][1], v_p_vec_arr[i_p][2],
+        v_p_ana_arr[i_p][0], v_p_ana_arr[i_p][1], v_p_ana_arr[i_p][2]
         );
   }
 
   print_bar();
   
+  // Print 'v_p' and its analytical counterpart
+  printf("jacobian[N_p]: \n");
+  printf(
+      "%5s\t%-45s%-45s\n", 
+      "i_p", "jac[i_p]", "jac_analytic[i_p]");
+
+  for (int i_p = 0; i_p < N_p; i_p++) {
+    printf("%5d\n",i_p);
+    printf("     \t\n");
+    for (int i_row = 0; i_row < DIM_R; i_row++) {
+      for (int i_col = 0; i_col < DIM_R; i_col++)
+      { printf("%15.5f", jac_p_arr[i_p][i_row][i_col]); }
+      for (int i_col = 0; i_col < DIM_R; i_col++)
+      { printf("%15.5f", jac_p_ana_arr[i_p][i_row][i_col]); }
+      printf("\n");
+    } printf("\n");
+  }
+  
 
   // Deallocate memory for arrays
-  delete [] r_p_arr[0];
-  delete [] r_p_arr;
-  delete [] v_p_arr[0];
-  delete [] v_p_arr;
+  delete [] r_p_vec_arr[0];
+  delete [] r_p_vec_arr;
+  delete [] v_p_vec_arr[0];
+  delete [] v_p_vec_arr;
   delete [] psi_arr[0];
   delete [] psi_arr;
+
+  delete [] jac_p_arr;
+  delete [] jac_p_ana_arr;
 
 
   //// Return from this program
